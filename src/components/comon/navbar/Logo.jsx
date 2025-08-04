da8930e39328bcf04655b3b3f67137a000f3c058
RadizJoster-web <radizdirganta0101@gmail.com>
2025-08-04 09:03:10 +0700

    modify logo size in nav

--- src/components/comon/navbar/Logo.jsx
+++ src/components/comon/navbar/Logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div>
       <Link to="/">
-        <h1 className="font-kanit text-3xl md:text-5xl">
+        <h1 className="font-kanit text-4xl md:text-5xl">
           Qui<span className="text-primary">ez</span>
         </h1>
       </Link>
